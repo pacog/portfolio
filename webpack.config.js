f52345eb813c90907264caf3eee29d14e7f32003
Paco Gomez Granados <pacogomezgranados@gmail.com>
2016-11-23 08:43:37 +0100

    Basic first config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = [
+  {
+    entry: './src/index.js',
+
+    output: {
+      filename: 'index.js',
+      path: 'dist',
+      publicPath: '/'
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.template.ejs',
+        inject: 'body',
+        options: {
+          title: 'pacog portfolio'
+        }
+      })
+    ],
+
+    module: {
+      loaders: [
+        { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+        { test: /\.(jpg|jpeg|png|gif|svg)$/, loader: 'file', exclude: /node_modules/ }
+      ]
+    }
+  }
+];
