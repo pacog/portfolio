29f7c838d745c1f361b5b9f95dd924c7d87aa87f
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-04-16 12:52:06 +0200

    Added file for capac racer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import './styles/main.scss';
-import 'file?name=./img/[name].[ext]!./img/bg.jpg';
-import 'file?name=./img/[name].[ext]!./img/doctortilla_portfolio.png';
-import 'file?name=./img/[name].[ext]!./img/chesire_portfolio.jpg';
-import 'file?name=./img/[name].[ext]!./img/polarsteps_portfolio.png';
-import 'file?name=./img/[name].[ext]!./img/recoreco_portfolio.png';
+import "./styles/main.scss";
+import "file?name=./img/[name].[ext]!./img/bg.jpg";
+import "file?name=./img/[name].[ext]!./img/doctortilla_portfolio.png";
+import "file?name=./img/[name].[ext]!./img/chesire_portfolio.jpg";
+import "file?name=./img/[name].[ext]!./img/polarsteps_portfolio.png";
+import "file?name=./img/[name].[ext]!./img/capac-racer_portfolio.png";
+import "file?name=./img/[name].[ext]!./img/recoreco_portfolio.png";
 
-import 'file?name=./[name].[ext]!./apple-touch-icon.png';
-import 'file?name=./[name].[ext]!./favicon-32x32.png';
-import 'file?name=./[name].[ext]!./favicon-16x16.png';
-import 'file?name=./[name].[ext]!./manifest.json';
-import 'file?name=./[name].[ext]!./safari-pinned-tab.svg';
+import "file?name=./[name].[ext]!./apple-touch-icon.png";
+import "file?name=./[name].[ext]!./favicon-32x32.png";
+import "file?name=./[name].[ext]!./favicon-16x16.png";
+import "file?name=./[name].[ext]!./manifest.json";
+import "file?name=./[name].[ext]!./safari-pinned-tab.svg";
 
-import { triangulator } from './js/triangulator';
+import { triangulator } from "./js/triangulator";
 
 const triangulation = triangulator({
-  url: './img/bg.jpg',
-  svg: document.querySelector('#svg-bg')
+  url: "./img/bg.jpg",
+  svg: document.querySelector("#svg-bg"),
 });
